8efbe83895b33bbb807dfbf7436087265014e360
Sakina Mammadzade <96643702+Sakhinamammadzade@users.noreply.github.com>
2024-04-02 22:51:44 +0400

    Update test_db_storage.py

--- tests/test_models/test_engine/test_db_storage.py
+++ tests/test_models/test_engine/test_db_storage.py
@@ -1,19 +1,18 @@
 #!/usr/bin/python3
-""" Module for testing database storage"""
+""" Test for DB storage """
+from os import getenv
 import unittest
 from models.base_model import BaseModel
-from models import storage, type_of_storage
-from os import getenv
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy import (create_engine)
+from models import storage
 
 
 @unittest.skipIf(getenv("HBNB_TYPE_STORAGE") != "db", "DBStorage")
 class TestDBStorage(unittest.TestCase):
-    """ Class to test the database storage method """
+    """ Test DBStorage class """
 
     def test_all(self):
-        """ method to test the all method of dbStorage """
+        """ test all method """
         new = BaseModel()
         temp = storage.all()
         self.assertIsInstance(temp, dict)
+
