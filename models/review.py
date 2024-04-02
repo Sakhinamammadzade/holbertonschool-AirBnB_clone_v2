4cf9253c4a7e7dfe3fd2c988add74c303c0df305
Sakina Mammadzade <96643702+Sakhinamammadzade@users.noreply.github.com>
2024-04-02 22:49:18 +0400

    Update review.py

--- models/review.py
+++ models/review.py
@@ -1,12 +1,27 @@
 #!/usr/bin/python3
-""" Review module for the HBNB project """
-from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, ForeignKey
+""" State Module for HBNB project """
+from models.base_model import BaseModel
+from models.base_model import Base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+from models.city import City
+from models.engine.file_storage import FileStorage
 
 
-class Review(BaseModel, Base):
-    """ Review classto store review information """
-    __tablename__ = 'reviews'
-    place_id = Column(String(60), ForeignKey("places.id"), nullable=False)
-    user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
-    text = Column(String(1024), nullable=False)
+class State(BaseModel, Base):
+    """ State class """
+    __tablename__ = 'states'
+
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", backref="state", cascade="all, delete")
+
+    def __init__(self, *args, **kwargs):
+        """initializes state"""
+        super().__init__(*args, **kwargs)
+
+    @property
+    def cities(self):
+        """Getter function for cities"""
+        storage = FileStorage()
+        return [city for city in storage.all(City).values()
+                if city.state_id == self.id]
