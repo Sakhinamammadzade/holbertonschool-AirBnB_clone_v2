e7d9466377fea7ad7896815a1c6ba8083a845600
Kamalusta <kamalusta@gmail.com>
2024-04-02 23:49:48 +0400

    update amenity

--- models/amenity.py
+++ models/amenity.py
@@ -3,6 +3,8 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+from models.place import place_amenity
+
 
 
 class Amenity(BaseModel, Base):
