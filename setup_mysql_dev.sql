ff21eec56f67cb51ea2de9684b2ef711211b1513
Sakina Mammadzade <96643702+Sakhinamammadzade@users.noreply.github.com>
2024-04-03 16:26:11 +0400

    Update setup_mysql_dev.sql

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,7 +1,14 @@
--- script that prepares a MySQL server for the projec
+-- create db
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
-CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost';
-SET PASSWORD FOR 'hbnb_dev'@'localhost' = 'hbnb_dev_pwd';
-GRANT USAGE ON *.* TO 'hbnb_dev'@'localhost';
-GRANT SELECT ON `performance_schema`.* TO 'hbnb_dev'@'localhost';
-GRANT ALL PRIVILEGES ON `hbnb_dev_db`.* TO 'hbnb_dev'@'localhost';
+
+-- create user
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+
+-- set privileges to user
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+
+-- set privileges to user
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
+
+-- apply changes
+FLUSH PRIVILEGES;
