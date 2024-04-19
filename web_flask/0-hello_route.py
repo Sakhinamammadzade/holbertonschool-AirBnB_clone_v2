518b78edeef7a97769fdd0a514ed046dd9d9a5d5
Sakhinamammadzade <sakhinamammadzade0406@gmail.com>
2024-04-19 10:32:15 +0400

    update 0-hello file add host and port

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,13 +1,16 @@
+#!/bin/python3
+"""starts Flask Applications"""
 from flask import Flask, request
-from markupsafe import escape
 
 
 app = Flask(__name__)
 
-@app.route('/')
+
+@app.route('/', strict_slashes=False)
 def hello():
     name = request.args.get("name", "HBNB!")
     return f'Hello, {escape(name)}!'
 
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)
