46bd0c7cbefe88a0b817a2ddac09bf67dcc9b6bc
Sakhinamammadzade <sakhinamammadzade0406@gmail.com>
2024-04-19 12:27:36 +0400

    Python is cool, i think it's lie

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -23,6 +23,7 @@ def c(text):
     return f"C {text}"
 
 
+@app.route('/python', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
 def python(text):
     text = text.replace("_", " ")
