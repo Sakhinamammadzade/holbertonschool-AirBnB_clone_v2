ae4c79bd4946ef7e9975c35e1a05b72f1915bce7
Sakhinamammadzade <sakhinamammadzade0406@gmail.com>
2024-04-19 12:30:06 +0400

    Python is cool

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -25,7 +25,7 @@ def c(text):
 
 @app.route('/python', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
-def python(text):
+def python(text="is cool"):
     text = text.replace("_", " ")
     return f"Python {text}"
 
