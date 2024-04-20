4b9610ca2d873952e1cd255ae63bb96962238174
Sakhinamammadzade <sakhinamammadzade0406@gmail.com>
2024-04-21 01:01:14 +0400

    render template

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -36,9 +36,9 @@ def number(n):
     return f'{n} is a number'
 
 
-@app.route('/number_template/<n>', strict_slashes=False)
+@app.route('/number_template/<int:n>', strict_slashes=False)
 def number_template(n):
-    return f'Number: {n}'
+    return render_template('5-number.html', n=n)
 
 
 if __name__ == '__main__':
