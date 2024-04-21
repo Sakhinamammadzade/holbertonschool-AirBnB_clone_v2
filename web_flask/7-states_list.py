89b4c86f1163eb2b5de151f062412d496963676d
Sakhinamammadzade <sakhinamammadzade0406@gmail.com>
2024-04-21 20:59:13 +0400

    import error fixed

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """Write a script that starts a Flask web application:"""
-from flask import Flask
-from models import Flask, render_template
+from flask import Flask, render_template
+from models import storage
 from models.state import State
 app = Flask(__name__)
 
